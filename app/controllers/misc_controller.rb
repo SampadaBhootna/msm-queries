2a601b078825d07f731a1c8423b3e092a947f989
SampadaBhootna <sbhootna@chicagobooth.edu>
2025-02-21 03:06:08 +0000

    Intermediate App

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,9 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors
+    @director_table = Director.all
+    render({ :template => "misc_templates/director"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  #Director Table
+  get("/directors", { :controller => "misc", :action => "directors" })
 end
